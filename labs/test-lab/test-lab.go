b84f94f137159eb2c0f45dc30fbfe471554708ca
Memohto <A01631327@itesm.mx>
2021-08-14 21:39:00 +0000

    Solve test-lab

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	if len(os.Args) == 1 {
+		fmt.Println("Error: You must send at least one parameter")
+		return
+	}
+	name := strings.Join(os.Args[1:], " ")
+	fmt.Printf("Hello %v, Welcome to the jungle \n", name)
 }
