a96d712f1bcc79a77b86a182cf799036bc772e6d
Memohto <A01631327@itesm.mx>
2021-09-08 00:02:39 +0000

    First partial solved

--- challenges/first-partial/c-month-day/month_day.c
+++ challenges/first-partial/c-month-day/month_day.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
 
 /* month_day function's prototype*/
-void month_day(int year, int yearday, int *pmonth, int *pday);
+void month_day(int year, int yearday, int *pmonth, int *pday) {
+    int leap;
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
 
-int main() {
+    int day = 0;
+    for (int i = 0; i < 13; i++) {
+        day += daytab[leap][i];
+        if(day >= yearday) {
+            day -= daytab[leap][i];
+            *pday = yearday - day;
+            *pmonth = i;
+            return;
+        }
+    }
+
+}
+
+char *month_name(int n) {
+
+   static char *name[] = {
+       "Illegal month",
+       "Jan", "Feb", "Mar",
+       "Apr", "May", "Jun",
+       "Jul", "Aug", "Sep",
+       "Oct", "Nov", "Dec"
+   };
+
+   return (n < 1 || n > 12) ? name[0] : name[n];
+}
+
+int main(int argc, char **argv) {
+    if(argc < 3) {
+        printf("Missing argument\n");
+        printf("Usage: %s <year> <yearday>\n", argv[0]);
+        return 0;
+    }
+
+    int month, day;
+    month_day(atoi(argv[1]), atoi(argv[2]), &month, &day);
+
+    if(month == 0 && day == 0) {
+        printf("Error: Invalid year day\n");
+    } else {
+        printf("%s %02d, %s\n", month_name(month), day, argv[1]);
+    }   
+    
     return 0;
 }
--- challenges/first-partial/go-functions-methods/geometry.go
+++ challenges/first-partial/go-functions-methods/geometry.go
@@ -7,18 +7,33 @@
 //!+point
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
 
-type Point struct{ X, Y float64 }
+type Point struct{ x, y float64 }
 
 // traditional function
-func Distance(p, q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
-}
+// func Distance(p, q Point) float64 {
+// 	return math.Hypot(q.X()-p.X(), q.Y()-p.Y())
+// }
 
 // same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.X()-p.X(), q.Y()-p.Y())
+}
+
+func (p Point) X() float64 {
+	return p.x
+}
+
+func (p Point) Y() float64 {
+	return p.y
 }
 
 //!-point
@@ -30,16 +45,152 @@ type Path []Point
 
 // Distance returns the distance traveled along the path.
 func (path Path) Distance() float64 {
+	fmt.Print("   - ")
 	sum := 0.0
+	dist := 0.0
 	for i := range path {
 		if i > 0 {
-			sum += path[i-1].Distance(path[i])
+			dist = path[i-1].Distance(path[i])
+			sum += dist
+			fmt.Printf("%.2f + ", dist)
 		}
 	}
+	dist = path[len(path)-1].Distance(path[0])
+	sum += dist
+	fmt.Printf("%.2f = %.2f\n", dist, sum)
 	return sum
 }
 
-func main() {
+//!-path
+
+// Given three colinear points p, q, r, the function checks if
+// point q lies on line segment 'pr'
+func onSegment(p, q, r Point) bool {
+	if q.x <= math.Max(p.X(), r.X()) && q.X() >= math.Min(p.X(), r.X()) &&
+		q.y <= math.Max(p.Y(), r.Y()) && q.Y() >= math.Min(p.Y(), r.Y()) {
+		return true
+	}
+
+	return false
 }
 
-//!-path
+// To find orientation of ordered triplet (p, q, r).
+// The function returns following values
+// 0 --> p, q and r are colinear
+// 1 --> Clockwise
+// 2 --> Counterclockwise
+func orientation(p, q, r Point) int {
+	val := (q.y-p.y)*(r.x-q.x) - (q.x-p.x)*(r.y-q.y)
+
+	if val == 0 {
+		return 0
+	}
+
+	if val > 0 {
+		return 1
+	} else {
+		return 2
+	}
+}
+
+// The main function that returns true if line segment 'p1q1'
+// and 'p2q2' intersect.
+func doIntersect(p1, q1, p2, q2 Point) bool {
+	o1 := orientation(p1, q1, p2)
+	o2 := orientation(p1, q1, q2)
+	o3 := orientation(p2, q2, p1)
+	o4 := orientation(p2, q2, q1)
+
+	// General case
+	if o1 != o2 && o3 != o4 {
+		return true
+	}
+
+	// Special Cases
+	// p1, q1 and p2 are colinear and p2 lies on segment p1q1
+	if o1 == 0 && onSegment(p1, p2, q1) {
+		return true
+	}
+
+	// p1, q1 and q2 are colinear and q2 lies on segment p1q1
+	if o2 == 0 && onSegment(p1, q2, q1) {
+		return true
+	}
+
+	// p2, q2 and p1 are colinear and p1 lies on segment p2q2
+	if o3 == 0 && onSegment(p2, p1, q2) {
+		return true
+	}
+
+	// p2, q2 and q1 are colinear and q1 lies on segment p2q2
+	if o4 == 0 && onSegment(p2, q1, q2) {
+		return true
+	}
+
+	// Doesn't fall in any of the above cases
+	return false
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	if len(os.Args) < 2 {
+		fmt.Printf("Missing argument\n")
+		fmt.Println("Usage: go run geometry.go <sides>")
+		return
+	}
+
+	sides, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if sides < 3 {
+		fmt.Println("You can't have a figure with less than 3 sides")
+		return
+	} else if sides > 50 {
+		fmt.Println("The sides limit is 50, chose a smaller number")
+		return
+	}
+
+	fmt.Printf(" - Generating a [%d] sides figure\n", sides)
+	figure := make(Path, sides)
+	for i := range figure {
+		if i > 2 {
+			generated := true
+			for generated {
+				flag := false
+				figure[i].x = -100 + rand.Float64()*200
+				figure[i].y = -100 + rand.Float64()*200
+				for j := range figure[:i-2] {
+					if doIntersect(figure[i-1], figure[i], figure[j], figure[j+1]) {
+						flag = true
+					}
+				}
+
+				if flag {
+					continue
+				}
+
+				for j := range figure[1 : i-1] {
+					if doIntersect(figure[i], figure[0], figure[j+1], figure[j+2]) {
+						flag = true
+					}
+				}
+				generated = flag
+			}
+		} else {
+			figure[i].x = -100 + rand.Float64()*200
+			figure[i].y = -100 + rand.Float64()*200
+		}
+	}
+
+	fmt.Println(" - Figure's vertices")
+	for _, p := range figure {
+		fmt.Printf("   - (%6.2f, %6.2f)\n", p.X(), p.Y())
+	}
+
+	fmt.Println(" - Figure's Perimeter")
+	figure.Distance()
+}
