437b924ef34c112c4f074caeedecffa152889e18
Memohto <A01631327@itesm.mx>
2021-09-28 15:14:24 +0000

    c-str-lib solved

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+int mystrcmp(char *a, char *b);
+
+int main(int argc, char **argv) {
+    if(argc != 4) {
+        printf("Incorrect usage\n");
+        printf("Option 1: %s -add <string 1> <string 2>\n", argv[0]);
+        printf("Option 2: %s -find <string> <substring>\n", argv[0]);
+        return 0;
+    }
+
+    if(mystrcmp(argv[1], "-add") == 0) {
+        printf("Initial Lenght: %d\n", mystrlen(argv[2]));
+        char *result = mystradd(argv[2], argv[3]);
+        printf("New String: %s\n", result);
+        printf("New length: %d\n", mystrlen(result));
+        free(result);
+    } else if(mystrcmp(argv[1], "-find") == 0) {
+        int index = mystrfind(argv[2], argv[3]);
+        if(index >= 0) {
+            printf("['%s'] string was found at [%d] position\n", argv[3], index);
+        } else {
+            printf("['%s'] string was not found\n", argv[3]);
+        }
+    } else {
+        printf("Incorrect usage\n");
+        printf("Option 1: %s -add <string 1> <string 2>\n", argv[0]);
+        printf("Option 2: %s -find <string> <substring>\n", argv[0]);
+        return 0;
+    }
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,47 @@
+ #include <stdlib.h>
 
-
-int mystrlen(char *str){
-    return 0;
+int mystrlen(char *str) {
+    int lenght = 0;
+    for(int i = 0; str[i] != '\0'; i++) {
+        lenght++;
+    }
+    return lenght;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char *mystradd(char *origin, char *addition) {
+    char *result = (char *)malloc(mystrlen(origin) + mystrlen(addition) + 1);
+    int i;
+    for(i = 0; origin[i] != '\0'; i++) {
+        result[i] = origin[i];
+    }
+    for(int j = 0; addition[j] != '\0'; j++) {
+        result[i] = addition[j];
+        i++;
+    }
+    return result;
 }
 
-int mystrfind(char *origin, char *substr){
-    return 0;
+int mystrfind(char *origin, char *substr) {
+    int i = 0, j = 0;
+    int matchFound = 0;
+    while(origin[i] != '\0' && !matchFound) {
+        j = 0;
+        while(substr[j] != '\0' && origin[i+j] == substr[j]) {
+            j++;
+        }
+        if(substr[j] == '\0') {
+            matchFound = 1;
+        } else {
+            i++;
+        }
+    }
+    return matchFound ? i : -1;
+}
+
+int mystrcmp(char *a, char *b) {
+    int equal = 0;
+    for(int i = 0; a[i] != '\0' && equal == 0; i++) {
+        if(a[i] != b[i]) equal = -1;
+    }
+    return equal;
 }
